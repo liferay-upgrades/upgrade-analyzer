60991212d50059965532d2035a09395cbc5b8d2d
Rafael Praxedes <rafael.praxedes@liferay.com>
2024-07-17 19:18:15 -0300

    ECU-2350 Implement equals/hashCode in Project model

--- src/main/java/com/liferay/upgrades/analyzer/project/dependency/model/Project.java
+++ src/main/java/com/liferay/upgrades/analyzer/project/dependency/model/Project.java
@@ -2,12 +2,27 @@ package com.liferay.upgrades.analyzer.project.dependency.model;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
 
 
 public class Project {
 
     private final ProjectKey projectKey;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Project)) return false;
+        Project project = (Project) o;
+        return Objects.equals(projectKey, project.projectKey);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(projectKey);
+    }
+
     private final Set<Project> dependencies = new HashSet<>();
 
     private Set<Project> consumers = new HashSet<>();
