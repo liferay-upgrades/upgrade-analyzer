6eed54efd2e467b282fc49eaa62126981c2ef5f6
Rafael Praxedes <rafael.praxedes@liferay.com>
2024-08-02 14:34:18 -0300

    ECU-2350 Update main class and improve exception handling

--- src/main/java/com/liferay/upgrades/analyzer/main/Main.java
+++ src/main/java/com/liferay/upgrades/analyzer/main/Main.java
@@ -3,37 +3,75 @@ package com.liferay.upgrades.analyzer.main;
 import com.liferay.upgrades.analyzer.project.dependency.analyzer.GradleProjectDependencyAnalyzer;
 import com.liferay.upgrades.analyzer.project.dependency.exporter.DOTProjectDependencyExporter;
 import com.liferay.upgrades.analyzer.project.dependency.exporter.GamePlanProjectDependencyExporter;
-import com.liferay.upgrades.analyzer.project.dependency.exporter.JSONProjectDependencyExporter;
-import com.liferay.upgrades.analyzer.project.dependency.exporter.ReverseDependencyTreeProjectDependencyExporter;
 import com.liferay.upgrades.analyzer.project.dependency.graph.builder.ProjectsDependencyGraph;
 
 import java.io.IOException;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
+        ExportOptions exportOptions = resolveOptions(args);
+
+        if (!exportOptions.gamePlan && !exportOptions.dotGraph) {
+            StringBuilder sb = new StringBuilder();
+
+            sb.append("The available options are:\n");
+            sb.append("\t--dot-graph or -g to export in the DOT graph format\n");
+            sb.append("\t--game-plan or -p to export the game plan\n");
+            sb.append("\t--directory or -d to specify the path for the liferay workspace\n");
+            sb.append("If just the /path/to/workspace is given, the output will be the same as -d /path/to/workspace -g");
+
+            System.out.println(sb.toString());
+
+            return;
+        }
+
         GradleProjectDependencyAnalyzer gradleProjectDependencyAnalyzer = new GradleProjectDependencyAnalyzer();
 
-        ProjectsDependencyGraph projectsDependencyGraph = gradleProjectDependencyAnalyzer.analyze(args[0]);
+        ProjectsDependencyGraph projectsDependencyGraph = gradleProjectDependencyAnalyzer.analyze(exportOptions.directory);
 
-        // System.out.println("// DOT Language //");
-        // System.out.println(new DOTProjectDependencyExporter().export(projectsDependencyGraph));
+        if  (exportOptions.gamePlan) {
+            System.out.println("// Game Plan //");
+            System.out.println(new GamePlanProjectDependencyExporter().export(projectsDependencyGraph));
+        }
 
-        // System.out.println("// JSON //");
-        // System.out.println(new JSONProjectDependencyExporter().export(projectsDependencyGraph));
-
-        System.out.println("// Game Plan //");
-        System.out.println(new GamePlanProjectDependencyExporter().export(projectsDependencyGraph));
-
-        // System.out.println("// Reverse Dependency Tree //");
-        // System.out.println(new ReverseDependencyTreeProjectDependencyExporter().export(projectsDependencyGraph));
+        if (exportOptions.dotGraph) {
+            System.out.println("// DOT Graph //");
+            System.out.println(new DOTProjectDependencyExporter().export(projectsDependencyGraph));
+        }
     }
 
-    private static String[] resolve() {
+    private static ExportOptions resolveOptions(String[] args) {
+        ExportOptions exportOptions = new ExportOptions();
 
-        return null;
+        if (args.length == 1 && !args[0].isBlank()) {
+            exportOptions.gamePlan = true;
+            exportOptions.directory = args[0];
+        }
+
+        for (int i = 0; i < args.length; i++) {
+            String arg = args[i];
+
+            if (arg.equals("--dot-graph") || arg.equals("-g")) {
+                exportOptions.dotGraph = true;
+            }
+            else if (arg.equals("--game-plan") || arg.equals("-p")) {
+                exportOptions.gamePlan = true;
+            } else if (arg.equals("--directory") || arg.equals("-d")) {
+                exportOptions.directory = args[i + 1];
+                i++;
+            }
+        }
+
+        return exportOptions;
     }
 
-    private class Option {
+    private static class ExportOptions {
+
+        boolean dotGraph;
+
+        boolean gamePlan;
+
+        String directory;
 
     }
 }
