0e548073762e090aaae4299d13ac5887928d801a
Rafael Praxedes <rafael.praxedes@liferay.com>
2024-10-02 17:05:42 -0300

    Create the ProjectCollector concept and implement to the existing detectors (Gradle and Theme projects)

---
+++ src/main/java/com/liferay/upgrades/analyzer/project/dependency/collector/GradleProjectCollector.java
@@ -0,0 +1,109 @@
+package com.liferay.upgrades.analyzer.project.dependency.collector;
+
+import com.liferay.upgrades.analyzer.project.dependency.graph.builder.ProjectsDependencyGraph;
+import com.liferay.upgrades.analyzer.project.dependency.graph.builder.ProjectsDependencyGraphBuilder;
+import com.liferay.upgrades.analyzer.project.dependency.model.ProjectKey;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class GradleProjectCollector implements ProjectCollector {
+    @Override
+    public boolean matches(String fileName, Path file) {
+
+        if (fileName.equals("build.gradle") &&
+                Files.exists(Paths.get(file.getParent().toString(), "src"))) {
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public void collect(Path file, ProjectsDependencyGraphBuilder projectsDependencyGraphBuilder) {
+        projectsDependencyGraphBuilder.addProject(
+                getProjectKey(
+                        file.getParent().toUri().getPath(),
+                        file.getParent().getFileName().toString()),
+                collectProjectDependencies(file));
+    }
+
+    private Set<ProjectKey> collectProjectDependencies(Path gradleFile) {
+        StringBuilder sb = new StringBuilder();
+
+        try (BufferedReader bufferedReader = Files.newBufferedReader(
+                gradleFile, StandardCharsets.UTF_8)) {
+
+            String line = null;
+            while ((line = bufferedReader.readLine()) != null) {
+                sb.append(line);
+                sb.append("\n");
+            }
+        }
+        catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        Set<ProjectKey> dependencies = new HashSet<>();
+
+        Matcher matcher = GRADLE_PROJECT_PATTERN.matcher(sb.toString());
+
+        while (matcher.find()) {
+            dependencies.add(getProjectKey(matcher.group(1)));
+        }
+
+        return dependencies;
+    }
+
+    private ProjectKey getProjectKey(String path, String rawProjectName) {
+        ProjectKey projectKey = getProjectKey(rawProjectName);
+
+        projectKey.setPath(path);
+
+        return projectKey;
+    }
+    private ProjectKey getProjectKey(String rawProjectName) {
+        String key = rawProjectName.trim().replaceAll("\'", "").replaceAll("\"", "");
+
+        if (key.contains(":")) {
+            ProjectKey projectKey = this.projectInfos.get(key);
+
+            if (projectKey != null) {
+                return projectKey;
+            }
+
+            String name = key.substring(key.lastIndexOf(":") + 1);
+
+            projectKey = this.projectInfos.remove(name);
+
+            if (projectKey == null) {
+                projectKey = new ProjectKey(name);
+            }
+
+            projectKey.setName(name);
+            projectKey.setKey(key);
+            projectKey.setGroup(key.substring(0, key.lastIndexOf(":")));
+
+            this.projectInfos.put(key, projectKey);
+            this.projectInfos.put(name, projectKey);
+
+            return projectKey;
+        }
+
+        return projectInfos.computeIfAbsent(key, name -> new ProjectKey(key));
+    }
+
+    private Map<String, ProjectKey> projectInfos = new HashMap<>();
+
+    private static final Pattern GRADLE_PROJECT_PATTERN = Pattern.compile("project.*\\(*[\"'](.*)[\"']\\)");
+}
