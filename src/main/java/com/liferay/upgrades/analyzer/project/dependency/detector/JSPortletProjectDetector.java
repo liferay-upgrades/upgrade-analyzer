c38dc4e9c58fcc9890d58874f6a090772397aedc
Albert <albert.gomes@liferay.com>
2024-11-05 09:58:08 -0300

    ECU-4755 Create JSPortletProjectDetector to Analyzer Tool

---
+++ src/main/java/com/liferay/upgrades/analyzer/project/dependency/detector/JSPortletProjectDetector.java
@@ -0,0 +1,52 @@
+package com.liferay.upgrades.analyzer.project.dependency.detector;
+
+import com.liferay.upgrades.analyzer.project.dependency.graph.builder.ProjectsDependencyGraphBuilder;
+import com.liferay.upgrades.analyzer.project.dependency.model.ProjectKey;
+import com.liferay.upgrades.analyzer.project.dependency.util.ProjectDetectorUtil;
+
+import java.nio.file.Path;
+import java.util.Collections;
+
+import org.json.JSONObject;
+
+public class JSPortletProjectDetector implements ProjectDetector {
+
+    @Override
+    public boolean matches(String fileName, Path file) {
+        return fileName.equals("package.json") && _validateJsonContent(file);
+    }
+
+    @Override
+    public void detect(Path file, ProjectsDependencyGraphBuilder projectsDependencyGraphBuilder) {
+        projectsDependencyGraphBuilder.addProject(
+                _getProjectKey(file),  Collections.emptySet());
+    }
+
+    private ProjectKey _getProjectKey(Path file) {
+        String path = file.getParent().toUri().getPath();
+        String fileName = file.getParent().getFileName().toString();
+
+        String key = ProjectDetectorUtil.normalize(fileName);
+
+        return new ProjectKey(key, path);
+    }
+
+    private boolean _validateJsonContent(Path contentPath) {
+        String jsonContent = ProjectDetectorUtil.readFile(contentPath);
+
+        if (jsonContent.isEmpty()) {
+            return false;
+        }
+
+        JSONObject jsonObject = new JSONObject(jsonContent);
+
+        if (jsonObject.has("portlet")) {
+            String jsonPortlet = jsonObject.get("portlet").toString();
+
+            return jsonPortlet.contains("com.liferay.portlet.display-category");
+        }
+
+        return false;
+    }
+
+}
