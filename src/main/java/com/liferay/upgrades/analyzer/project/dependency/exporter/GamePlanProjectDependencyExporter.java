58e9843d24484c15f737dddd0782a7bf60ce8ea0
Rafael Praxedes <rafael.praxedes@liferay.com>
2024-11-21 14:02:08 -0300

    Use a non-recursive approach to do a BFS on the dependencyGraph

--- src/main/java/com/liferay/upgrades/analyzer/project/dependency/exporter/GamePlanProjectDependencyExporter.java
+++ src/main/java/com/liferay/upgrades/analyzer/project/dependency/exporter/GamePlanProjectDependencyExporter.java
@@ -1,5 +1,6 @@
 package com.liferay.upgrades.analyzer.project.dependency.exporter;
 
+import com.liferay.upgrades.analyzer.project.dependency.exporter.util.ExporterUtil;
 import com.liferay.upgrades.analyzer.project.dependency.graph.builder.ProjectsDependencyGraph;
 import com.liferay.upgrades.analyzer.project.dependency.model.Project;
 
@@ -8,56 +9,25 @@ import java.io.File;
 import java.io.FileWriter;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Comparator;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
-import java.util.TreeMap;
 import java.util.stream.Collectors;
-
-import static com.liferay.upgrades.analyzer.project.dependency.exporter.util.ExporterUtil.visitConsumers;
 
 public class GamePlanProjectDependencyExporter implements ProjectDependencyExporter<String> {
     @Override
     public String export(ProjectsDependencyGraph projectsDependencyGraph) {
         StringBuilder sb = new StringBuilder();
 
-        Set<Project> allProjects = new HashSet<>();
+        Map<Integer, Set<Project>> projectsMapLevels = ExporterUtil.createProjectLevel(projectsDependencyGraph);
 
-        visitConsumers(1, projectsDependencyGraph.getLeaves(), (level, project) -> {
-            allProjects.add(project);
-            Set<Project> projects = _projectsMapLevels.computeIfAbsent(level, key -> new HashSet<>());
-
-            for (Map.Entry<Integer, Set<Project>> entry : _projectsMapLevels.entrySet()) {
-
-                if (entry.getValue().contains(project)) {
-                    if (entry.getKey() <= level) {
-                        entry.getValue().remove(project);
-                        break;
-                    }
-                    else {
-                        return;
-                    }
-                }
-            }
-
-            projects.add(project);
-        });
-
-        sb.append("This project contains " + allProjects.size() + " projects with " +
-                _projectsMapLevels.size() + " level(s) of project dependencies.");
+        sb.append("This project contains " + ExporterUtil.countProjects(projectsMapLevels)+ " projects with " +
+                projectsMapLevels.size() + " level(s) of project dependencies.");
         sb.append("\n");
-
-        Comparator<Project> projectsComparator = Comparator.comparingInt(
-                (Project p1) -> -p1.getConsumers().size()
-        ).thenComparing(
-                Project::getName
-        );
 
         int level = 1;
 
-        for (Map.Entry<Integer, Set<Project>> entry : _projectsMapLevels.entrySet()) {
+        for (Map.Entry<Integer, Set<Project>> entry : projectsMapLevels.entrySet()) {
             List<Project> projects = new ArrayList<>(entry.getValue());
 
             if (projects.size() == 0) {
@@ -67,7 +37,7 @@ public class GamePlanProjectDependencyExporter implements ProjectDependencyExpor
             sb.append("Level " + level++ + " count: " + projects.size());
             sb.append("\n");
 
-            Collections.sort(projects, projectsComparator);
+            Collections.sort(projects, ExporterUtil.getProjectsComparator());
 
             for (Project project : projects) {
                 sb.append("\t");
@@ -76,7 +46,6 @@ public class GamePlanProjectDependencyExporter implements ProjectDependencyExpor
                 sb.append("\n");
             }
         }
-
 
         long time = System.currentTimeMillis();
 
@@ -113,5 +82,4 @@ public class GamePlanProjectDependencyExporter implements ProjectDependencyExpor
     }
 
 
-    private Map<Integer, Set<Project>> _projectsMapLevels = new TreeMap<>();
 }
--- src/main/java/com/liferay/upgrades/analyzer/project/dependency/exporter/util/ExporterUtil.java
+++ src/main/java/com/liferay/upgrades/analyzer/project/dependency/exporter/util/ExporterUtil.java
@@ -1,11 +1,21 @@
 package com.liferay.upgrades.analyzer.project.dependency.exporter.util;
 
+import com.liferay.upgrades.analyzer.project.dependency.graph.builder.ProjectsDependencyGraph;
 import com.liferay.upgrades.analyzer.project.dependency.model.Project;
 
+import java.util.Comparator;
+import java.util.HashSet;
+import java.util.Map;
 import java.util.Set;
+import java.util.Stack;
+import java.util.TreeMap;
 import java.util.function.BiConsumer;
 
 public class ExporterUtil {
+
+    public static Comparator<Project> getProjectsComparator() {
+        return projectsComparator;
+    }
 
     public static void visitConsumers(int level, Set<Project> projects, BiConsumer<Integer, Project> doVisit) {
         for (Project project : projects) {
@@ -14,4 +24,78 @@ public class ExporterUtil {
             visitConsumers(level + 1, project.getConsumers(), doVisit);
         }
     }
+
+    public static int countProjects(Map<Integer, Set<Project>> projectsMapLevels) {
+        int count = 0;
+
+        for (Set<Project> projects : projectsMapLevels.values()) {
+            count += projects.size();
+        }
+        return count;
+    }
+
+    public static Map<Integer, Set<Project>> createProjectLevel(ProjectsDependencyGraph projectsDependencyGraph) {
+        Map<Integer, Set<Project>> projectsMapLevels = new TreeMap<>();
+
+        Set<Project> allProjects = new HashSet<>();
+
+        Stack<Project> currentLevel = new Stack<>();
+
+        projectsDependencyGraph.getLeaves().forEach(currentLevel::push);
+
+        Stack<Project> nextLevel = new Stack<>();
+
+        int level = 1;
+
+        while (!currentLevel.isEmpty()) {
+            Project project = currentLevel.pop();
+
+            nextLevel.addAll(project.getConsumers());
+
+            allProjects.add(project);
+
+            if (checkLevels(level, project, projectsMapLevels)) {
+                Set<Project> currentLevelProjects = projectsMapLevels.computeIfAbsent(level, key -> new HashSet<>());
+
+                currentLevelProjects.add(project);
+            }
+
+            if (currentLevel.isEmpty()) {
+                currentLevel.addAll(nextLevel);
+
+                nextLevel.removeAllElements();
+
+                level++;
+            }
+        }
+
+        return projectsMapLevels;
+
+    }
+
+    private static boolean checkLevels(int level, Project project, Map<Integer, Set<Project>> projectsMapLevels) {
+        boolean addToLevel = true;
+
+        for (Map.Entry<Integer, Set<Project>> entry : projectsMapLevels.entrySet()) {
+
+            if (entry.getValue().contains(project)) {
+                if (entry.getKey() <= level) {
+                    entry.getValue().remove(project);
+                }
+                else {
+                    addToLevel = false;
+                }
+
+                break;
+            }
+        }
+
+        return addToLevel;
+    }
+
+   private static Comparator<Project> projectsComparator = Comparator.comparingInt(
+            (Project p1) -> -p1.getConsumers().size()
+    ).thenComparing(
+            Project::getName
+    );
 }
