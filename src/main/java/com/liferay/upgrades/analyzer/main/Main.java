f54f4ebe1a8259a7ce59e2eb15cde72831943533
Albert <albert.gomes@liferay.com>
2025-05-15 19:45:42 -0300

    ECU-9277 Prevent required parameter exceptions, add short description to parameters and create a method to generate report when parameters is not used properly

--- src/main/java/com/liferay/upgrades/analyzer/main/Main.java
+++ src/main/java/com/liferay/upgrades/analyzer/main/Main.java
@@ -3,6 +3,7 @@ package com.liferay.upgrades.analyzer.main;
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
 import com.beust.jcommander.ParameterDescription;
+import com.beust.jcommander.ParameterException;
 import com.liferay.upgrades.analyzer.project.dependency.analyzer.ProjectDependencyAnalyzer;
 import com.liferay.upgrades.analyzer.project.dependency.analyzer.ProjectStartupUniquifier;
 import com.liferay.upgrades.analyzer.project.dependency.deployer.LocalShell;
@@ -15,7 +16,6 @@ import com.liferay.upgrades.analyzer.project.dependency.exporter.*;
 import com.liferay.upgrades.analyzer.project.dependency.graph.builder.ProjectsDependencyGraph;
 import com.liferay.upgrades.analyzer.project.dependency.model.Project;
 
-import java.io.IOException;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.Map;
@@ -24,75 +24,71 @@ import java.util.logging.Logger;
 public class Main {
 
     public static void main(String[] args) {
-        ExportOptions exportOptions = _resolveOptions(args);
+        try {
+            ExportOptions exportOptions = _resolveOptions(args);
 
-        if (!exportOptions.gamePlan && !exportOptions.dotGraph &&
-                !exportOptions.startupGamePlan && !exportOptions.moduleDeployer) {
-            StringBuilder sb = new StringBuilder();
+            if (exportOptions.moduleDeployer) {
+                ModuleDeployer moduleDeployer = new ModuleDeployer();
 
-            sb.append("The available options are:\n");
-            sb.append("\t--dot-graph or -d to export in the DOT graph format\n");
-            sb.append("\t--game-plan or -p to export the game plan\n");
-            sb.append("\t--startup-game-plan or -stp to export the startup game plan\n");
-            sb.append("\t--module-deploy or -md to deploy a module and its submodules\n");
-            sb.append("\tIn the -md option you need to specify the path to the module you want to deploy, ");
-            sb.append("ie. -md -f /path/to/workspace/modules/lorem-ipsum-module\n");
-            sb.append("\t--folder or -f to specify the path for the liferay workspace\n");
-            sb.append("If just the /path/to/workspace is given, the output will be the same as -p -f /path/to/workspace");
+                String script = moduleDeployer.scriptFactory(
+                    Paths.get(exportOptions.directory + "/build.gradle"));
 
-            System.out.println(sb);
-
-            return;
-        }
-
-        if (exportOptions.moduleDeployer) {
-            ModuleDeployer moduleDeployer = new ModuleDeployer();
-
-            String script = moduleDeployer.scriptFactory(
-                Paths.get(exportOptions.directory + "/build.gradle"));
-
-            try {
                 LocalShell.executeCommand(script);
             }
-            catch (IOException ex) {
-                log.severe("Error = " + ex.getMessage());
+            else {
+                ProjectDependencyAnalyzer projectDependencyAnalyzer =
+                        new ProjectDependencyAnalyzer(
+                                List.of(
+                                        new GradleProjectDetector(), new MavenProjectDetector(),
+                                        new ThemeProjectDetector(), new JSPortletProjectDetector()));
+
+                ProjectsDependencyGraph projectsDependencyGraph =
+                        projectDependencyAnalyzer.analyze(exportOptions.directory);
+
+                if  (exportOptions.gamePlan) {
+                    System.out.println(
+                            new GamePlanProjectDependencyExporter().export(projectsDependencyGraph));
+                    System.out.println(
+                            new CsvProjectDependencyExporter().export(projectsDependencyGraph));
+                }
+
+                if (exportOptions.dotGraph) {
+                    System.out.println(
+                            new DOTProjectDependencyExporter().export(projectsDependencyGraph));
+                    System.out.println(
+                            new CsvProjectDependencyExporter().export(projectsDependencyGraph));
+                }
+
+                if (exportOptions.startupGamePlan) {
+                    List<List<Project>> uniqueProjects =
+                            new ProjectStartupUniquifier().uniquify(projectsDependencyGraph);
+
+                    System.out.println(
+                            new StartupGamePlanProjectDependecyExporter().export(uniqueProjects));
+                    System.out.println(
+                            new StartupCsvProjectDependencyExporter().export(uniqueProjects));
+                }
             }
         }
-        else {
-            ProjectDependencyAnalyzer projectDependencyAnalyzer =
-                new ProjectDependencyAnalyzer(
-                    List.of(
-                        new GradleProjectDetector(), new MavenProjectDetector(),
-                        new ThemeProjectDetector(), new JSPortletProjectDetector()));
-
-            ProjectsDependencyGraph projectsDependencyGraph =
-                projectDependencyAnalyzer.analyze(exportOptions.directory);
-
-            if  (exportOptions.gamePlan) {
-                System.out.println(
-                    new GamePlanProjectDependencyExporter().export(projectsDependencyGraph));
-                System.out.println(
-                    new CsvProjectDependencyExporter().export(projectsDependencyGraph));
+        catch (Exception exception) {
+            if (exception instanceof ParameterException) {
+                log.info(_generateOptionsHelp());
             }
-
-            if (exportOptions.dotGraph) {
-                System.out.println(
-                    new DOTProjectDependencyExporter().export(projectsDependencyGraph));
-                System.out.println(
-                    new CsvProjectDependencyExporter().export(projectsDependencyGraph));
-            }
-
-            if (exportOptions.startupGamePlan) {
-                List<List<Project>> uniqueProjects =
-                    new ProjectStartupUniquifier().uniquify(projectsDependencyGraph);
-
-                System.out.println(
-                    new StartupGamePlanProjectDependecyExporter().export(uniqueProjects));
-                System.out.println(
-                    new StartupCsvProjectDependencyExporter().export(uniqueProjects));
-            }
+            else throw new RuntimeException(exception);
         }
 
+    }
+
+    private static String _generateOptionsHelp() {
+        return "The available options are:\n" +
+                "\t--dot-graph or -d to export in the DOT graph format\n" +
+                "\t--game-plan or -p to export the game plan\n" +
+                "\t--startup-game-plan or -stp to export the startup game plan\n" +
+                "\t--module-deploy or -md to deploy a module and its submodules\n" +
+                "\tIn the -md option you need to specify the path to the module you want to deploy, " +
+                "ie. -md -f /path/to/workspace/modules/lorem-ipsum-module\n" +
+                "\t--folder or -f to specify the path for the liferay workspace (Required)\n" +
+                "If just the /path/to/workspace is given, the output will be the same as -p -f /path/to/workspace";
     }
 
     private static ExportOptions _resolveOptions(String[] args) {
@@ -122,25 +118,41 @@ public class Main {
 
     private static class ExportOptions {
 
-        @Parameter(names = {"-dot-graph", "-d"})
+        @Parameter(
+            names = {"-d", "--dot-graph"},
+            description = "Export in the DOT graph format"
+        )
         boolean dotGraph;
 
-        @Parameter(names = {"--game-plan", "-p"})
+        @Parameter(
+            names = {"-p", "--game-plan"},
+            description = "Export the game plan"
+        )
         boolean gamePlan;
 
-        @Parameter(names = {"--startup-game-plan", "-stp"})
+        @Parameter(
+            names = {"-stp", "--startup-game-plan"},
+            description = "Export the startup game plan"
+        )
         boolean startupGamePlan;
 
-        @Parameter(names = {"--module-deploy", "-md"})
+        @Parameter(
+            names = {"-md", "--module-deploy"},
+            description = "Deploy a module and its submodules"
+        )
         boolean moduleDeployer;
 
-        @Parameter(names = {"--folder", "-f"}, required = true)
+        @Parameter(
+            names = {"-f", "--folder"},
+            description = "Specify the path for the liferay workspace",
+            required = true
+        )
         String directory;
 
         public Map<String, Boolean> exporters() {
             return Map.of(
-                    "dot-graph", dotGraph, "game-plan", gamePlan,
-                    "startup-game-plan", startupGamePlan
+                "dot-graph", dotGraph, "game-plan", gamePlan,
+                "startup-game-plan", startupGamePlan
             );
         }
 
