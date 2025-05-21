300a487032fd5fcb00d739c9d114a79747a74f6e
Albert Gomes <albert.gomes@liferay.com>
2025-05-21 16:31:54 -0300

    NOISSUE Refactory StartupCsvProjectDependencyExporter exporter (WIP)

--- src/main/java/com/liferay/upgrades/analyzer/project/dependency/exporter/StartupCsvProjectDependencyExporter.java
+++ src/main/java/com/liferay/upgrades/analyzer/project/dependency/exporter/StartupCsvProjectDependencyExporter.java
@@ -1,89 +1,70 @@
 package com.liferay.upgrades.analyzer.project.dependency.exporter;
 
+import com.liferay.upgrades.analyzer.project.dependency.detector.APIModuleProjectDetector;
+import com.liferay.upgrades.analyzer.project.dependency.detector.FragmentHostModuleProjectDetector;
+import com.liferay.upgrades.analyzer.project.dependency.detector.ServiceModuleProjectDetector;
+import com.liferay.upgrades.analyzer.project.dependency.graph.builder.ProjectsDependencyGraph;
 import com.liferay.upgrades.analyzer.project.dependency.model.Project;
 
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 import java.util.*;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
 
-public class StartupCsvProjectDependencyExporter {
+public class StartupCsvProjectDependencyExporter implements ProjectDependencyExporter<String> {
 
-    private final Map<Integer,String> startupLevelsTitle = new HashMap<Integer, String>(){{
-        put(0,"Services and APIs");
-        put(1,"Utils");
-        put(2,"Commons");
-        put(3,"Hooks");
-        put(4,"Fragments");
-        put(5,"Others");
-    }};
-
-    public String export(List<List<Project>> uniqueProjects) {
+    @Override
+    public String export(ProjectsDependencyGraph projectsDependencyGraph) {
         StringBuilder sb = new StringBuilder();
 
-        sb.append("Level," +
-                "Bundle Name," +
-                "Symbolic Name," +
-                "Dependencies," +
-                "Nº of compile errors before automation," +
-                "Nº of compile errors after automation," +
-                "Automation suggested fix?," +
-                "Nº of errors fixed by suggestions," +
-                "Nº of errors fixed (Total)," +
-                "Obs");
-        sb.append("\n");
+        sb.append("Startup Game Plan with the proposed levels").append("\n\n");
 
-        int level = 1;
+        Map<String, StringBuilder> categoryMap = new LinkedHashMap<>();
 
-        for (int i = 0; i < 6; i++) {
-            if(!uniqueProjects.get(i).isEmpty()) {
-                for (Project project : uniqueProjects.get(i)) {
-                    sb.append(startupLevelsTitle.get(i));
-                    sb.append(",");
-                    sb.append(project.getName());
-                    sb.append(",");
-                    sb.append(project.getName());
-                    sb.append(",");
-                    sb.append(
-                            Stream.of(
-                                    project.getDependencies()
-                            ).flatMap(
-                                    Set::stream
-                            ).map(
-                                    p -> p.getName()
-                            ).collect(
-                                    Collectors.joining( " ")
-                            )
-                    );
-                    sb.append(",");
-                    sb.append("1");
-                    sb.append(",");
-                    sb.append("0");
-                    sb.append(",");
-                    sb.append("FALSE");
-                    sb.append(",");
-                    sb.append(",");
-                    sb.append("0");
-                    sb.append(",");
-                    sb.append("\n");
-                }
+        categoryMap.put("Service and API", new StringBuilder());
+        categoryMap.put("Fragment Host", new StringBuilder());
+        categoryMap.put("Others", new StringBuilder());
+
+        for (Project project : projectsDependencyGraph.getLeaves()) {
+            String projectCategory = project.getName().split("=")[1];
+            String projectName = project.getName().split("=")[0];
+
+            if (projectCategory.equals(APIModuleProjectDetector.class.getSimpleName()) ||
+                    projectCategory.equals(ServiceModuleProjectDetector.class.getSimpleName())) {
+
+                _appendByCategory(categoryMap, "Service and API", projectName);
+            }
+            else if (projectCategory.equals(FragmentHostModuleProjectDetector.class.getSimpleName())) {
+                _appendByCategory(categoryMap, "Fragment Host", projectName);
+            }
+            else {
+                _appendByCategory(categoryMap, "Others", projectName);
             }
         }
 
-        long time = System.currentTimeMillis();
-
-        File csvFile = new File("projects-" + time + ".csv");
-
-        try (BufferedWriter writer = new BufferedWriter(new FileWriter(csvFile))) {
-            writer.write(sb.toString());
+        for (Map.Entry<String, StringBuilder> entry : categoryMap.entrySet()) {
+            sb.append(entry.getKey()).append(":\n\n");
+            sb.append(entry.getValue()).append("\n");
         }
-        catch (Exception e) {
-            throw new RuntimeException(e);
+
+        File csvFile = new File("projects-" + System.currentTimeMillis() + ".csv");
+
+        try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(csvFile))) {
+            bufferedWriter.write(sb.toString());
+        }
+        catch (IOException ioException) {
+            throw new RuntimeException(ioException);
         }
 
         return "CSV file generated at " + csvFile.getAbsolutePath();
+    }
+
+    public static void _appendByCategory(
+        Map<String, StringBuilder> categoryMap, String category, String content) {
+
+        categoryMap.computeIfAbsent(
+            category, k -> new StringBuilder()).append(content).append("\n");
     }
 
 }
