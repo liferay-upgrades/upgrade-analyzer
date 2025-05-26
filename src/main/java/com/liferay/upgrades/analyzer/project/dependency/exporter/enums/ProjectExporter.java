e717e116a31551c5355d1772ec8a4e81bd56ef22
Albert Gomes <albert.gomes@liferay.com>
2025-05-26 10:37:45 -0300

    ECU-9955 Log levels

--- src/main/java/com/liferay/upgrades/analyzer/project/dependency/exporter/enums/ProjectExporter.java
+++ src/main/java/com/liferay/upgrades/analyzer/project/dependency/exporter/enums/ProjectExporter.java
@@ -3,34 +3,41 @@ package com.liferay.upgrades.analyzer.project.dependency.exporter.enums;
 import com.liferay.upgrades.analyzer.project.dependency.exporter.*;
 import com.liferay.upgrades.analyzer.project.dependency.graph.builder.ProjectsDependencyGraph;
 
+import java.util.logging.Logger;
+
 public enum ProjectExporter {
 
     DOT_GRAPH {
         @Override
         public void export(ProjectsDependencyGraph projectsDependencyGraph) {
-            System.out.println(
+            _logger.info(
                 new DOTProjectDependencyExporter().export(projectsDependencyGraph));
-            System.out.println(
+            _logger.info(
                 new GamePlanProjectDependencyExporter().export(projectsDependencyGraph));
         }
     },
     GAME_PLAN {
         @Override
         public void export(ProjectsDependencyGraph projectsDependencyGraph) {
-            System.out.println(
+            _logger.info(
                 new CsvProjectDependencyExporter().export(projectsDependencyGraph));
-            System.out.println(
+            _logger.info(
                 new GamePlanProjectDependencyExporter().export(projectsDependencyGraph));
         }
     },
     STARTUP_GAME_PLAN {
         @Override
         public void export(ProjectsDependencyGraph projectsDependencyGraph) {
-            System.out.println(new StartupCsvProjectDependencyExporter().export(projectsDependencyGraph));
-            System.out.println(new StartupGamePlanProjectDependecyExporter());
+            _logger.info(
+                new StartupCsvProjectDependencyExporter().export(projectsDependencyGraph));
+            _logger.info(
+                new StartupGamePlanProjectDependencyExporter().export(projectsDependencyGraph));
         }
     };
 
     public abstract void export(ProjectsDependencyGraph projectsDependencyGraph);
+
+    private static final Logger _logger = Logger.getLogger(
+        ProjectExporter.class.getName());
 
 }
